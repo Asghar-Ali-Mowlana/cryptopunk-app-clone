e4fd3f5dc625c358e9d678659fea1f63072afaae
Asghar Ali Mowlana <alimowlana24@gmail.com>
2022-06-07 18:51:08 +0400

    Designed the punk list and connected and pulled data from the api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import PunkList from "./components/PunkList";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    try {
+      const getMyNfts = async () => {
+        const openseaData = await axios.get(
+          "https://testnets-api.opensea.io/assets?asset_contract_address=0x2E7C542DB2079F67602FEA30cBF6d9c2564eF744&order_direction=asc"
+        );
+        console.log(openseaData.data.assets);
+        setPunkListData(openseaData.data.assets);
+      };
+      getMyNfts();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://gateway.thirdweb.dev/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg"
-      />
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
